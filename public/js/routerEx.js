b49871c324b7df9e71af6439e09bb225c1676374
Santosh <santosharakere@gmail.com>
2017-03-24 17:07:19 +0000

    optional demo

---
+++ public/js/routerEx.js
@@ -0,0 +1,80 @@
+var TodoRouter = Backbone.Router.extend({
+    /* define the route and function maps for this router */
+    routes: {
+        "about" : "showAbout",
+        /* Sample usage: http://example.com/#about */
+
+        "todo/:id" : "getTodo",
+        /* This is an example of using a ":param" variable which allows us to match
+        any of the components between two URL slashes */
+        /* Sample usage: http://example.com/#todo/5 */
+
+        "search/:query" : "searchTodos",
+        /* We can also define multiple routes that are bound to the same map function,
+        in this case searchTodos(). Note below how we're optionally passing in a
+        reference to a page number if one is supplied */
+        /* Sample usage: http://example.com/#search/job */
+
+        "search/:query/p:page" : "searchTodos",
+        /* As we can see, URLs may contain as many ":param"s as we wish */
+        /* Sample usage: http://example.com/#search/job/p1 */
+
+        "todos/:id/download/*documentPath" : "downloadDocument",
+        /* This is an example of using a *splat. Splats are able to match any number of
+        URL components and can be combined with ":param"s*/
+        /* Sample usage: http://example.com/#todos/5/download/files/Meeting_schedule.doc */
+
+        /* If you wish to use splats for anything beyond default routing, it's probably a good
+        idea to leave them at the end of a URL otherwise you may need to apply regular
+        expression parsing on your fragment */
+
+        "optional(/:item)": "optionalItem",
+        "named/optional/(y:z)": "namedOptionalItem",
+        /* Router URLs also support optional parts via parentheses, without having
+           to use a regex.  */
+
+        "*other"    : "defaultRoute",
+        /* This is a default route that also uses a *splat. Consider the
+        default route a wildcard for URLs that are either not matched or where
+        the user has incorrectly typed in a route path manually */
+        /* Sample usage: http://example.com/# <anything> */
+
+    },
+
+    showAbout: function(){
+        debugger
+        console.log('...showAbout')
+    },
+
+    getTodo: function(id){
+        /*
+        Note that the id matched in the above route will be passed to this function
+        */
+        console.log("You are trying to reach todo " + id);
+    },
+
+    searchTodos: function(query, page){
+        var page_number = page || 1;
+        console.log("Page number: " + page_number + " of the results for todos containing the word: " + query);
+    },
+
+    downloadDocument: function(id, path){
+        console.log(`id=${id}`)
+        console.log(`path=${path}`)
+    },
+
+    defaultRoute: function(other){
+        console.log('Invalid. You attempted to reach:' + other);
+    },
+
+    optionalItem: function(item) {
+        console.log('optional route...')
+        console.log(`item = ${item}`)
+    }
+});
+
+/* Now that we have a router setup, we need to instantiate it */
+debugger
+var myTodoRouter = new TodoRouter()
+
+Backbone.history.start()
